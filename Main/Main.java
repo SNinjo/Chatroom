774e703cca6bbc1b8283ea883219fab22e4488f9
SNinjo <70991369+SNinjo@users.noreply.github.com>
2022-04-16 19:27:35 +0800

    update 2022/4/16

--- Main/Main.java
+++ Main/Main.java
@@ -6,14 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         // GUI 多人連線
-        /*Server.Main.excute(new int[]{120, 480});
-        Client.Main.excute("鍾承翰", new int[]{10, 100});
-        Client.Main.excute("王俊傑", new int[]{535, 100});
-        Client.Main.excute("廖儒均", new int[]{1060, 100});*/
+        Server.Main.excute(new int[]{120, 500});
+        Client.Main.excute("User01", new int[]{10, 100});
+        Client.Main.excute("User02", new int[]{535, 100});
+        Client.Main.excute("User03", new int[]{1060, 100});
 
         // Client - Server
-        Server.Main.excute(new int[]{535, 100});
-        Client.Main.excute("使用者", new int[]{10, 100});
+//        Server.Main.excute(new int[]{535, 100});
+//        Client.Main.excute("使用者", new int[]{10, 100});
     }
 
 }
